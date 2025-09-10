ab76c017d442938ffe3c0a0ca47f4fcde10011c3
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-10 17:03:34 +0330

    ✨ header, input and btn added

---
+++ todo/src/components/header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+    <div>
+        <h1>
+            Todo<span className="flex text-[rgba(141, 224, 200, 1)] justify-center align-middle text-center">List</span>
+        </h1>
+    </div>
+)
+}
---
+++ todo/src/components/index.jsx
@@ -0,0 +1,15 @@
+import { Btn } from "./btn";
+import { Header } from "./header";
+import { Input } from "./input";
+
+export function Index() {
+    return (
+    <div>
+        <Header/>
+        <div>
+            <Input/>
+            <Btn/>
+        </div>
+    </div>
+)
+}
