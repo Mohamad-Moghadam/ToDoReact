7de99ee287682b2bf59560dbb980a0d766c337dd
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-09-12 16:56:08 +0330

    ✨ package added

--- todo/src/components/index.jsx
+++ todo/src/components/index.jsx
@@ -4,7 +4,7 @@ import { Input } from "./input";
 
 export function Index() {
     return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex justify-center content-center">
         <Header/>
         <div>
             <Input/>
